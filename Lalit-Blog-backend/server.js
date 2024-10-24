30937a3afaeb964a395a600619859dc62bc807e4
Lalit kumar <139781963+Lalitk52221@users.noreply.github.com>
2024-10-24 20:19:25 +0530

    fix

--- Lalit-Blog-backend/server.js
+++ Lalit-Blog-backend/server.js
@@ -5,7 +5,7 @@ dotenv.config({path:"./config.env"});
 mongoose.connect(process.env.DB_URL).then(()=>{
     console.log("DB Connected")
 })
-PORT_NO = process.env.PORT_NO;
+PORT_NO = process.env.PORT_NO || 4000;
 app.listen(PORT_NO,()=>{
     console.log("Successfully Created on - ",PORT_NO," port Number")
 })
